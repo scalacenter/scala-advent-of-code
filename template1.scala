5fff69d516da8592f0377fb8a943fd74ab86f821
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-11-29 16:58:44 +0100

    Add template1 solution

---
+++ template1.scala
@@ -0,0 +1,24 @@
+// using scala 3.0.1
+
+package template1
+
+import scala.io.Source
+
+@main def part1(args: String*): Unit =
+  val path = args.headOption.getOrElse("input/template1.part1")
+  val input = Source.fromFile(path).mkString
+  val answer = computeAnswer(input, 2)
+  println(s"The solution of is $answer")
+
+@main def part2(args: String*): Unit =
+  val path = args.headOption.getOrElse("input/template1.part2")
+  val input = Source.fromFile(path).mkString
+  val answer = computeAnswer(input, 2)
+  println(s"The solution is $answer")
+
+private def computeAnswer(input: String, n: Int): String =
+  val entries = input.split('\n').map(_.toInt).toSeq
+  val combinations = entries.combinations(n)
+  combinations.find(_.sum == 2020)
+    .map(_.product.toString)
+    .getOrElse(throw new Exception("No solution found"))
