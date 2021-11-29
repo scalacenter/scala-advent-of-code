fbd46b9d94b6c7efdbce4d15ccaffda9ace740d9
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-11-29 18:42:44 +0100

    Fix edit url function on docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
           path: 'target/mdoc',
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/scalacenter/scala-advent-of-code/edit/main/website',
+          editUrl: param => `https://github.com/scalacenter/scala-advent-of-code/edit/website/docs/${param.docPath}`,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
