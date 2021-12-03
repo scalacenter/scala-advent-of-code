c8d4c0891655cde072ef2f6f2bd21b2758e3d689
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-12-03 16:28:25 +0100

    Tweak the reading of the file for day 3.

--- src/day3.scala
+++ src/day3.scala
@@ -7,21 +7,17 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation._
 
 @main def part1(): Unit =
-  val input = readInput()
+  val input = readFileSync("input/day3", "utf-8")
   val answer = part1(input)
   println(s"The solution is $answer")
 
 @main def part2(): Unit =
-  val input = readInput()
+  val input = readFileSync("input/day3", "utf-8")
   val answer = part2(input)
   println(s"The solution is $answer")
 
-def readInput(): String =
-  NodeFS.readFileSync("input/day3", "utf-8")
-
-object NodeFS:
-  @js.native @JSImport("fs", "readFileSync")
-  def readFileSync(path: String, charset: String): String = js.native
+@js.native @JSImport("fs", "readFileSync")
+def readFileSync(path: String, charset: String): String = js.native
 
 def part1(input: String): Int =
   val bitLines: List[BitLine] = input.linesIterator.map(parseBitLine).toList
