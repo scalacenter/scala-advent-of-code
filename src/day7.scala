04c45c16d1f4a2ac0c12cc33c002aa25f9de8fc6
tgodzik <tgodzik@virtuslab.com>
2021-12-07 10:54:50 +0100

    Refactor day 7 solution according to suggestions

--- src/day7.scala
+++ src/day7.scala
@@ -1,5 +1,4 @@
 // using scala 3.1.0
-// using lib com.lihaoyi::pprint:0.6.6
 
 package day7
 
@@ -7,19 +6,32 @@ import scala.util.Using
 import scala.io.Source
 import scala.annotation.tailrec
 
-case class Crabmarine(
-    horizontal: Int,
-    increasingCost: Boolean,
-    fuelCost: Int = 1
-):
-  def moveUp() =
-    if increasingCost then
-      this.copy(horizontal = horizontal + 1, fuelCost = fuelCost + 1)
-    else this.copy(horizontal = horizontal + 1)
-  def moveDown() =
-    if increasingCost then
-      this.copy(horizontal = horizontal -1, fuelCost = fuelCost + 1)
-    else this.copy(horizontal = horizontal - 1)
+@main def part1(): Unit =
+  println(s"The solution is ${part1(readInput())}")
+
+@main def part2(): Unit =
+  println(s"The solution is ${part2(readInput())}")
+
+def readInput(): String =
+  Using.resource(Source.fromFile("input/day7"))(_.mkString)
+
+sealed trait Crabmarine:
+  def moveForward(): Crabmarine
+  def moveBackward(): Crabmarine
+  def horizontal: Int
+  def fuelCost: Int
+
+case class ConstantCostCrabmarine(horizontal: Int) extends Crabmarine:
+  def fuelCost: Int = 1
+  def moveForward(): Crabmarine = this.copy(horizontal = horizontal + 1)
+  def moveBackward(): Crabmarine = this.copy(horizontal = horizontal - 1)
+
+case class IncreasingCostCrabmarine(horizontal: Int, fuelCost: Int = 1)
+    extends Crabmarine:
+  def moveForward() =
+    this.copy(horizontal = horizontal + 1, fuelCost = fuelCost + 1)
+  def moveBackward() =
+    this.copy(horizontal = horizontal - 1, fuelCost = fuelCost + 1)
 
 case class Crabmada(crabmarines: List[Crabmarine]):
 
@@ -33,42 +45,45 @@ case class Crabmada(crabmarines: List[Crabmarine]):
     val allTheSame = situation.forall(_.horizontal == situation.head.horizontal)
     if allTheSame then fuelCost
     else
-      val topHorizontal = situation.maxBy(_.horizontal)
+      val maxHorizontal = situation.maxBy(_.horizontal)
       val minHorizontal = situation.minBy(_.horizontal)
 
-      val fuelCostForTop = situation.collect {
-        case p if p.horizontal == topHorizontal.horizontal => p.fuelCost
+      val fuelCostForMax = situation.collect {
+        case crabmarine if crabmarine.horizontal == maxHorizontal.horizontal =>
+          crabmarine.fuelCost
       }.sum
       val fuelCostForMin = situation.collect {
-        case p if p.horizontal == minHorizontal.horizontal => p.fuelCost
+        case crabmarine if crabmarine.horizontal == minHorizontal.horizontal =>
+          crabmarine.fuelCost
       }.sum
-      if fuelCostForTop < fuelCostForMin then
-        val updated = situation.map { p =>
-          if p.horizontal == topHorizontal.horizontal then p.moveDown()
-          else p
+      if fuelCostForMax < fuelCostForMin then
+        val updated = situation.map { crabmarine =>
+          if crabmarine.horizontal == maxHorizontal.horizontal then
+            crabmarine.moveBackward()
+          else crabmarine
         }
-        align(updated, fuelCost + fuelCostForTop)
+        align(updated, fuelCost + fuelCostForMax)
       else
 
-        val updated = situation.map { p =>
-          if p.horizontal == minHorizontal.horizontal then p.moveUp()
-          else p
+        val updated = situation.map { crabmarine =>
+          if crabmarine.horizontal == minHorizontal.horizontal then
+            crabmarine.moveForward()
+          else crabmarine
         }
         align(updated, fuelCost + fuelCostForMin)
+      end if
     end if
   end align
 end Crabmada
 
-@main
-def main =
-  val input = 
-    Using.resource(Source.fromFile("input/day7"))(_.mkString).split(",").map(_.toInt).toList
-  val crabmarines1 = input.map(p => Crabmarine(p, increasingCost = false))
-  val part1 = Crabmada(crabmarines1).align()
-  pprint.log(part1)
+def part1(input: String): Int =
+  val horizontalPositions = input.split(",").map(_.toInt).toList
+  val crabmarines =
+    horizontalPositions.map(horizontal => ConstantCostCrabmarine(horizontal))
+  Crabmada(crabmarines).align()
 
-  val crabmarines2 = input.map(p => Crabmarine(p, increasingCost = true))
-  val part2 = Crabmada(crabmarines2).align()
-  pprint.log(part2)
-
-end main
+def part2(input: String): Int =
+  val horizontalPositions = input.split(",").map(_.toInt).toList
+  val crabmarines =
+    horizontalPositions.map(horizontal => IncreasingCostCrabmarine(horizontal))
+  Crabmada(crabmarines).align()
