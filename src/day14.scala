a87a24aa8f11eb9b29701648ea0747f816b02ba4
Sébastien Doeraene <sjrdoeraene@gmail.com>
2021-12-14 10:50:04 +0100

    Add more types, and use Longs to prepare part 2.

--- src/day14.scala
+++ src/day14.scala
@@ -16,34 +16,37 @@ import scala.io.Source
 def readInput(): String =
   Using.resource(Source.fromFile("input/day14"))(_.mkString)
 
-type InsertionRules = Map[(Char, Char), Char]
+type Polymer = List[Char]
+type CharPair = (Char, Char)
+type InsertionRules = Map[CharPair, Char]
+type Frequencies = Map[Char, Long]
 
-def part1(input: String): Int =
+def part1(input: String): Long =
   val (initialPolymer, insertionRules) = parseInput(input)
   val finalPolymer = (0 until 10).foldLeft(initialPolymer)((polymer, _) => applyRules(polymer, insertionRules))
-  val frequencies = finalPolymer.groupMapReduce(identity)(_ => 1)(_ + _)
+  val frequencies: Frequencies = finalPolymer.groupMapReduce(identity)(_ => 1L)(_ + _)
   val max = frequencies.values.max
   val min = frequencies.values.min
   max - min
 
-def parseInput(input: String): (List[Char], InsertionRules) =
+def parseInput(input: String): (Polymer, InsertionRules) =
   val sections = input.split("\n\n")
   val initialPolymer = sections(0).toList
   val insertionRules = sections(1).linesIterator.map(parseRule).toMap
   (initialPolymer, insertionRules)
 
-def parseRule(line: String): ((Char, Char), Char) =
+def parseRule(line: String): (CharPair, Char) =
   line match
     case s"$pairStr -> $inserStr" => (pairStr(0), pairStr(1)) -> inserStr(0)
     case _                        => throw new Exception(s"Cannot parse '$line' as an insertion rule")
 
-def applyRules(polymer: List[Char], rules: InsertionRules): List[Char] =
+def applyRules(polymer: Polymer, rules: InsertionRules): Polymer =
   val pairs = polymer.zip(polymer.tail)
-  val insertionsAndSeconds =
+  val insertionsAndSeconds: List[List[Char]] =
     for pair @ (first, second) <- pairs
       yield rules(pair) :: second :: Nil
   polymer.head :: insertionsAndSeconds.flatten
 
-def part2(input: String): Int =
+def part2(input: String): Long =
   val (initialPolymer, insertionRules) = parseInput(input)
   ???
