80e760963892bee245bdc387ca5590edd50d1f40
Mewen Crespo <mcrespopro@gmail.com>
2022-12-10 13:07:41 +0100

    Update day10.scala

--- 2022/src/day10.scala
+++ 2022/src/day10.scala
@@ -5,7 +5,9 @@ import inputs.Input.loadFileSync
 
 import Direction.*
 
-def INPUT(): String = loadFileSync(s"$currentDir/../input/day10")
+@main def part1(): Unit = println(s"The solution is ${part1(loadInput())}")
+@main def part2(): Unit = println(s"The CRT output is:\n${part2(loadInput())}")
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day10")
 
 enum Command {
   case NOOP
@@ -14,35 +16,33 @@ enum Command {
 
 export Command.*
 
-def commandsIterator: Iterator[Command] = for (line <- INPUT.linesIterator) yield line.strip match {
+def commandsIterator(input: String): Iterator[Command] = for (line <- input.linesIterator) yield line.strip match {
   case "noop" => NOOP
   case s"addx $x" if x.toIntOption.isDefined => ADDX(x.toInt)
   case _ => throw IllegalArgumentException(s"Invalid command '$line''")
 }
 
-val REGISTER_START_VALUE = 1
+val RegisterStartValue = 1
 
-def registerValuesIterator: Iterator[Int] = {
-  commandsIterator.scanLeft(REGISTER_START_VALUE :: Nil) {
+def registerValuesIterator(input: String): Iterator[Int] = {
+  commandsIterator(input).scanLeft(RegisterStartValue :: Nil) {
     case (_ :+ value, NOOP) => value :: Nil
     case (_ :+ value, ADDX(x)) => value :: value + x :: Nil
   }
 }.flatten
 
-def registerStrengthsIterator: Iterator[Int] = {
-  val it = for ((reg, i) <- registerValuesIterator.zipWithIndex) yield (i + 1) * reg
+def registerStrengthsIterator(input: String): Iterator[Int] = {
+  val it = for ((reg, i) <- registerValuesIterator(input).zipWithIndex) yield (i + 1) * reg
   it.drop(19).grouped(40).map(_.head)
 }
 
-@main def part1(): Unit = println(s"The solution is ${registerStrengthsIterator.sum}")
+def part1(input: String): Int = registerStrengthsIterator(input).sum
 
-val CRT_WIDTH: Int = 40
+val CRTWidth: Int = 40
 
-def CRTCharIterator: Iterator[Char] =
-  for ((reg, crt_pos) <- registerValuesIterator.zipWithIndex) yield {
-    if ((reg - (crt_pos % CRT_WIDTH)).abs <= 1) '#' else '.'
+def CRTCharIterator(input: String): Iterator[Char] =
+  for ((reg, crt_pos) <- registerValuesIterator(input).zipWithIndex) yield {
+    if ((reg - (crt_pos % CRTWidth)).abs <= 1) '#' else '.'
   }
 
-def CRTImage: String = CRTCharIterator.grouped(CRT_WIDTH).map(_.mkString).mkString("\n")
-
-@main def part2(): Unit = println(s"The CRT output is:\n$CRTImage")
+def part2(input: String): String = CRTCharIterator(input).grouped(CRTWidth).map(_.mkString).mkString("\n")
