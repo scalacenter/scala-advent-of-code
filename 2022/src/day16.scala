641511bcf12886d13ddbf4a345961bc704c0b6c1
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-11-23 18:19:52 +0100

    Make the day16 of 2022 implementation fastish on Scala.js.
    (and really fast on the JVM)

--- 2022/src/day16.scala
+++ 2022/src/day16.scala
@@ -19,7 +19,7 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
    http://www.apache.org/licenses/LICENSE-2.0
-   
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ def computeRoutes(start: Id, neighbors: Id => List[Id]): Map[Id, Int] =
 
   case class State(frontier: List[(Id, Int)], scores: Map[Id, Int]):
 
-    private def getScore(id: Id): Int = scores.getOrElse(id, Int.MaxValue) 
+    private def getScore(id: Id): Int = scores.getOrElse(id, Int.MaxValue)
     private def setScore(id: Id, s: Int) = State((id, s + 1) :: frontier, scores + (id -> s))
 
     def dequeued: (Id, State) =
@@ -74,7 +74,7 @@ def computeRoutes(start: Id, neighbors: Id => List[Id]): Map[Id, Int] =
 
   object State:
     def initial(start: Id) = State(List((start, 0)), Map(start -> 0))
-  
+
   def recurse(state: State): State =
     if state.frontier.isEmpty then
       state
@@ -98,27 +98,36 @@ def bestPath(map: RoomsInfo, start: Id, valves: Set[Id], timeAllowed: Int): Int 
   // we limit our options by only considering the still-closed valves
   // and `valves` has already culled any room with a flow value of 0 -- no point in considering these rooms!
 
-  def recurse(path: List[Id], valvesLeft: Set[Id], timeLeft: Int, totalValue: Int): Int =
+  val valvesArray = valves.toArray
+  val valveCount = valvesArray.length
+  val valvesIndicesLeft = Array.fill[Boolean](valveCount)(true)
+  val roomsByIndices = Array.tabulate(valveCount)(i => map.rooms(valvesArray(i)))
+
+  def recurse(current: Id, timeLeft: Int, totalValue: Int): Int =
     // recursively consider all plausible options
     // we are finished when we no longer have time to reach another valve or all valves are open
-    valvesLeft
-      .flatMap{ id =>
-        val current  = path.head
-        val distance = map.routes(current)(id)
+    val routesOfCurrent = map.routes(current)
+    var bestValue = totalValue
+    for index <- 0 until valveCount do
+      if valvesIndicesLeft(index) then
+        val id = valvesArray(index)
+        val distance = routesOfCurrent(id)
         // how much time is left after we traverse there and open the valve?
         val t = timeLeft - distance - 1
         // if `t` is zero or less this option can be skipped
-        Option.when(t > 0) {
+        if t > 0 then
           // the value of choosing a particular valve (over the life of our simulation)
           // is its flow rate multiplied by the time remaining after opening it
-          val value = map.rooms(id).flow * t
-          recurse(id :: path, valvesLeft - id, t, totalValue + value)
-        }
-      }
-      .maxOption
-      .getOrElse { totalValue }
+          val value = roomsByIndices(index).flow * t
+          valvesIndicesLeft(index) = false
+          val recValue = recurse(id, t, totalValue + value)
+          valvesIndicesLeft(index) = true
+          if recValue > bestValue then
+            bestValue = recValue
+    end for
+    bestValue
   end recurse
-  recurse(start :: Nil, valves, timeAllowed, 0)
+  recurse(start, timeAllowed, 0)
 
 def part1(input: String) =
   val time   = 30
