5396c9bf2f0ac4a389529d2d9bfbb2f41db092f7
Jamie Thompson <bishbashboshjt@gmail.com>
2023-12-05 12:32:47 +0100

    loosen css rules

--- website/src/components/Literate.js
+++ website/src/components/Literate.js
@@ -3,10 +3,14 @@ import React from 'react'
 const Literate = ({ children }) => {
   // see https://stackoverflow.com/a/59431317
   const StyledChildren = () =>
-    React.Children.map(children, child =>
-      React.cloneElement(child, {
-        className: `${child.props.className} ${"literate"}`
-      })
+    React.Children.map(children, child => {
+        const newClassName =
+          child.props.className === undefined ?
+            "literate-coding" : `${child.props.className} ${"literate-coding"}`;
+        return React.cloneElement(child, {
+          className: newClassName
+        })
+      }
     );
 
   return <StyledChildren />;
