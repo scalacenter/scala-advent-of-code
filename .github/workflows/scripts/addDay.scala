2f2855154a371f0ebcf679b116985e87ce9efd29
Seth Tisue <seth@tisue.net>
2025-11-18 15:18:11 -0500

    modernize syntax

--- .github/workflows/scripts/addDay.scala
+++ .github/workflows/scripts/addDay.scala
@@ -1,7 +1,7 @@
-//> using scala "3.7.4"
-//> using lib "com.lihaoyi::sourcecode:0.4.4"
-//> using lib "com.lihaoyi::os-lib:0.11.6"
-//> using lib "org.jsoup:jsoup:1.15.4"
+//> using scala 3.7.4
+//> using dep com.lihaoyi::sourcecode:0.4.4
+//> using dep com.lihaoyi::os-lib:0.11.6
+//> using dep org.jsoup:jsoup:1.15.4
 
 import java.time.LocalDate
 import java.time.Month
